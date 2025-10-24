fd45fbd05948bc8f9b9b72b4e9326389db7212e4
jguardo03 <jguardoruiz@gmail.com>
2025-10-24 12:31:49 +1100

    test

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeFOpaUwaFECdxZsiGXVHUyo7ycfBuCsg",
+    authDomain: "portfolio-9da15.firebaseapp.com",
+    projectId: "portfolio-9da15",
+    storageBucket: "portfolio-9da15.firebasestorage.app",
+    messagingSenderId: "818550472669",
+    appId: "1:818550472669:web:9aed576a6b0716860b8034",
+    measurementId: "G-9RCQPMJQG9"
+};
